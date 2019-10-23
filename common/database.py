a40f270801b4e90288ab5a035dfa88f011bce1b3
OrykV <36900781+OrykV@users.noreply.github.com>
2019-10-23 14:03:01 +0300

    Update database.py

--- common/database.py
+++ common/database.py
@@ -8,7 +8,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['heroku_2w3vn51g']
 
     @staticmethod
     def insert(collection, data):
