305217ccbdc44398976f1361a6961e976af96e79
OrykV <11paravoz22@gmail.com>
2019-10-24 12:18:24 +0300

    Uri corrected

--- common/database.py
+++ common/database.py
@@ -2,15 +2,14 @@ import pymongo
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
-    # URI = "mongodb://User1:test1234@ds237588.mlab.com:37588/heroku_2w3vn51g"
+    URI = "mongodb://User1:test1234@ds237588.mlab.com:37588/heroku_2w3vn51g"
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        # Database.DATABASE = client.get_default_database()
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client.get_default_database()
+
 
     @staticmethod
     def insert(collection, data):
